34f2f994b03e6e31ce876b84c522cc33ca357f0f
shambu2 <mugalishambu601@gmail.com>
2025-09-22 21:01:07 +0530

    added collection component

--- components/Collection.tsx
+++ components/Collection.tsx
@@ -4,8 +4,8 @@ const Collection = () => {
   return (
     <div>
         <div className='flex flex-col items-center text-center my-10 gap-4'>
-            <h1 className='font-bold text-5xl space-x-4'><span className='text-gray-600'>Latest</span>Collections <div className='w-10 h-[2px] bg-white'></div></h1>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur ipsum nemo iure nobis.</p>
+            <h1 className='font-bold text-5xl '><span className='text-gray-500 pr-4'>Latest</span>Collections </h1>
+            <p className='text-lg'>Discover the latest trends in fashion. Quality fabrics, unique designs, and styles that suit every occasion.</p>
         </div>
         <div>
 
